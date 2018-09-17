bb3cd5413e01c3bfcf159fe92dcafd20cf51bb9c
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-09-17 15:18:41 -0300

    More changes.

--- back/database.py
+++ back/database.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python
 import MySQLdb
+import datetime
 
 db = MySQLdb.connect(host="localhost",    # your host, usually localhost
                      user="root",         # your username
@@ -12,7 +13,8 @@ cur = db.cursor()
 
 # Use all the SQL you like
 # cur.execute("INSERT INTO test VALUES ('light', 2)")
-cur.execute("INSERT INTO test VALUES (%s, %s)", ('temp', 2))
+values = get_humidity_temperature() + get_gas_light()
+cur.execute("INSERT INTO data VALUES (%s, %s, %s, %s, %s, %s, %s)", (datetime.datetime.now(), values[0], values[1], values[2], values[3] ,values[4], values[5]))
 cur.execute("SELECT * FROM test")
 
 # print all the first cell of all the rows
