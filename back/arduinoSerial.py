8cc6da62e0df25d133c069dea0dfee2644a9c8fa
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-09-24 14:29:03 -0300

    Small changes.

--- back/arduinoSerial.py
+++ back/arduinoSerial.py
@@ -5,5 +5,4 @@ def get_gas_light():
 	"""Returns a list with four values, LPG amount, CO amount, Smoke and light value, in that order."""
 	result = ser.readline()
 	print(result)
-	list[first:last]
 	return result.split(";")[0:5]
