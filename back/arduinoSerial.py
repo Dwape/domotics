bfcd496cd6c2f7376bce9f6927230a3e9c52bffa
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-09-24 14:11:10 -0300

    Testing.

--- back/arduinoSerial.py
+++ back/arduinoSerial.py
@@ -3,4 +3,6 @@ ser = serial.Serial('/dev/ttyUSB0')
 
 def get_gas_light():
 	"""Returns a list with four values, LPG amount, CO amount, Smoke and light value, in that order."""
-	return ser.readline().split(";")
+	result = ser.readline()
+	print(result)
+	return result.split(";")
