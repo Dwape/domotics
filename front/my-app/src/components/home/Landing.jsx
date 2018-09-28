29247c2a78bb035b8c130c5dcc31fad5c7ba5e73
frank-117 <franco.velardez@ing.austral.edu.ar>
2018-09-28 19:45:24 -0300

    added Sass and home bg image

--- front/my-app/src/components/home/Landing.jsx
+++ front/my-app/src/components/home/Landing.jsx
@@ -3,10 +3,8 @@ import React, {Component} from 'react';
 class Landing extends Component {
     render() {
         return (
-            <div className="container-fluid bg-primary">
-                <div className="row">
-                    landing
-                </div>
+            <div>
+
             </div>
         )
     }
