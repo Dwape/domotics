d461b5e898e0a5d0f69c014d557f2e9bd48123ab
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-10-01 14:33:17 -0300

    Changes.

--- back/webapp/app.py
+++ back/webapp/app.py
@@ -9,7 +9,14 @@ def hello():
     connect()
     values = get_latest_values()[0]
     date = values[0].strftime("%Y-%m-%d %H:%M:%S")
-    return date
+    json = "{\"datetime\" =" + date + ","
+    json = json + "\"humidity\" =" + values[1] + ","
+    json = json + "\"temperature\" =" + values[2] + ","
+    json = json + "\"LPG\" =" + values[3] + ","
+    json = json + "\"CO\" =" + values[4] + ","
+    json = json + "\"smoke\" =" + values[5] + ","
+    json = json + "\"light\" =" + values[6] + "}"
+    return json
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80, debug=True)
