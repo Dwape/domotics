fcf08a8ce0ce03452637634f3592cb78f2c4e4b3
Dwape <eduardo.lalor@ing.austral.edu.ar>
2018-11-24 11:51:48 -0300

    Fixed identation in testlogger.py.

--- back/webapp/testlogger.py
+++ back/webapp/testlogger.py
@@ -10,29 +10,27 @@ Logs values into the database.
 Sleeps so that measurements are saved every 30 seconds
 '''
 
-connect()
-
 previous_values = None
 
 while True:
-	try:
-		values = get_humidity_temperature() + get_gas_light()
-		if values[0] == None or values[1] == None:
-			if previous_values != None:
-				values[0] = previous_values[0]
-				values[1] = previous_values[1]
-				#save_values(values)
+    try:
+        values = get_humidity_temperature() + get_gas_light()
+        if values[0] == None or values[1] == None:
+            if previous_values != None:
+                values[0] = previous_values[0]
+                values[1] = previous_values[1]
+                #save_values(values)
                 save(values) # using HTTP
-				print("Values saved")
-				previous_values = values
-		else:
-			previous_values = values
-			#save_values(values)
+                print("Values saved")
+                previous_values = values
+        else:
+            previous_values = values
+            #save_values(values)
             save(values) # using HTTP
-			print("Values saved")
-		time.sleep(30)
-	except RuntimeError:
-		close_connection()
+            print("Values saved")
+        time.sleep(30)
+    except RuntimeError:
+        close_connection()
 
 def save(values):
     data = [
